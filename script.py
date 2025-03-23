1eef50e6ab55ceae58d6ef3618e7d5d0c5900e57
Nick Byrne <byrnen8@tcd.ie>
2025-03-23 16:16:51 -0300

    Add uv script

---
+++ script.py
@@ -0,0 +1,17 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [ "jatic_dota" ]
+# [tool.uv.sources]
+# jatic_dota = { git = "https://github.com/nenb/jatic-dota.git", rev = "main" }
+# ///
+
+import subprocess
+import sys
+
+IMG_PATH = "path/to/dota/image.png"
+
+if __name__ == "__main__":
+    try:
+        subprocess.run([sys.executable, "-m", "jatic_dota", "--image_path", IMG_PATH], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running jatic_dota: {e}")

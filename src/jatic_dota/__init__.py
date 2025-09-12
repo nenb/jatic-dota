775035e04c3f04ce455f7e05f395602c59449902
Nick Byrne <byrnen8@tcd.ie>
2025-09-12 15:52:56 -0300

    Add CI

--- src/jatic_dota/__init__.py
+++ src/jatic_dota/__init__.py
@@ -1,3 +1,10 @@
+import sys
+try:
+    import detectron2  # user-provided install
+except Exception:
+    from jatic_dota._vendor import detectron2 as _vendored_d2
+    sys.modules.setdefault("detectron2", _vendored_d2)
+
 from jatic_dota.inference import bbav_inference, dafne_inference, initialize_bbav_model, initialize_dafne_model
 from jatic_dota.plot import plot_bounding_boxes
 
